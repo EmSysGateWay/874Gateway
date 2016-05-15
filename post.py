db8cdebf70e04c53449b0d85c2774b869fd92045
hedejing <hedejin95@gmail.com>
2016-05-15 22:13:11 +0800

    python post request

---
+++ post.py
@@ -0,0 +1,18 @@
+import requests
+import json
+import time
+
+# url = 'http://localhost'
+url = 'http://112.124.2.37'
+data = {"auth_id": "id","auth_key": "key","device_id": "id"}
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+report = requests.post(url+'/api/report', data=json.dumps(data), headers=headers)
+print report.status_code
+
+count = 1
+s = data['auth_id'] 
+while count <= 10:
+    data['auth_id'] += str(count)
+    poll = requests.post(url+'/api/poll', data=json.dumps(data), headers=headers)
+    time.sleep(1)
+    count += 1    
