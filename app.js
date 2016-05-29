d8221aaf5ec19a23ace302520ca0c34d4cb9ee47
hedejing <hedejin95@gmail.com>
2016-05-29 14:55:56 +0800

    update post to server

--- app.js
+++ app.js
@@ -6,16 +6,13 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
 var title = '<h1><meta charset="utf-8"> 一个非常简单的页面</meta></h1>\n';
 var report = {
-    time: '-',
     "auth_id": "",
     "auth_key": "",
     "device_id": ""
 }
 var poll = {
-    time: '-',
     "auth_id": "",
     "auth_key": "",
     "device_id": ""
@@ -39,23 +36,42 @@ app.post('/poll', function(req, res){
 app.post('/api/report', function (req, res) {
     console.log(req.body);
     var d = new Date();
-    report['time'] = d.toDateString() + ' ' + d.toTimeString();
+    report['update_time'] = d.toDateString() + ' ' + d.toTimeString();
+    //show report content
     report['auth_id'] = req.body['auth_id'];
     report['auth_key'] = req.body['auth_key'];
     report['device_id'] = req.body['device_id'];
+    report['payload'] = req.body['payload'];
     res.send('OK');
 });
 
 app.post('/api/poll', function (req, res) {
-    // console.log(req.body);
-    // var d = new Date();
-    // poll['time'] = d.toDateString() + ' ' + d.toTimeString();
-    // poll['auth_id'] = req.body['auth_id'];
-    // poll['auth_key'] = req.body['auth_key'];
-    // poll['device_id'] = req.body['device_id'];
-    // res.send('OK');
-    res.send(message);
-    message = [];
+
+    if(req.body['device_id'] == '869'){
+        if(req.body['payload']['RequestType'] == 'auth' ){
+            res.status(200).json({
+                code:0,
+                ResponseType:'auth',
+                DeviceId:'869',
+                Ids:[
+                    {id:'id1'},
+                    {id:'id2'}
+                ]
+            });
+        }
+        else if(req.body['payload']['RequestType'] == 'upload' ){
+            res.status(200).json({
+                code:0,
+                ResponseType:'upload',
+                DeviceId:'869',
+                Success:'y'
+            });
+        }
+        else
+            res.sendStatus(403);
+    }
+    else
+        res.sendStatus(403);
 });
 
 app.listen(80, function () {
