ef3c391334fd35b6aedb0528323b082ce451b00e
hedejing <hedejin95@gmail.com>
2016-05-29 12:00:55 +0800

    update server

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ 
-  extended: true
-})); 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 
 var title = '<h1><meta charset="utf-8"> 一个非常简单的页面</meta></h1>\n';
 var report = {
@@ -20,22 +21,25 @@ var poll = {
     "device_id": ""
 }
 
+var message = [];
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/report', function (req, res) {
+app.post('/report', function (req, res) {
     res.json(report);
 });
 
-app.get('/poll', function (req, res) {
-    res.json(poll);
-});
+app.post('/poll', function(req, res){
+    message.push(req.body);
+    console.log('after poll', message);
+})
 
 app.post('/api/report', function (req, res) {
     console.log(req.body);
     var d = new Date();
-    report['time'] =  d.toDateString() + ' ' + d.toTimeString();
+    report['time'] = d.toDateString() + ' ' + d.toTimeString();
     report['auth_id'] = req.body['auth_id'];
     report['auth_key'] = req.body['auth_key'];
     report['device_id'] = req.body['device_id'];
@@ -43,13 +47,15 @@ app.post('/api/report', function (req, res) {
 });
 
 app.post('/api/poll', function (req, res) {
-    console.log(req.body);
-    var d = new Date();
-    poll['time'] = d.toDateString() + ' ' + d.toTimeString();
-    poll['auth_id'] = req.body['auth_id'];
-    poll['auth_key'] = req.body['auth_key'];
-    poll['device_id'] = req.body['device_id'];
-    res.send('OK');
+    // console.log(req.body);
+    // var d = new Date();
+    // poll['time'] = d.toDateString() + ' ' + d.toTimeString();
+    // poll['auth_id'] = req.body['auth_id'];
+    // poll['auth_key'] = req.body['auth_key'];
+    // poll['device_id'] = req.body['device_id'];
+    // res.send('OK');
+    res.send(message);
+    message = [];
 });
 
 app.listen(80, function () {
--- post.py
+++ post.py
@@ -2,8 +2,8 @@ import requests
 import json
 import time
 
-# url = 'http://localhost'
-url = 'http://112.124.2.37'
+url = 'http://localhost'
+#url = 'http://112.124.2.37'
 data = {"auth_id": "id","auth_key": "key","device_id": "id"}
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 report = requests.post(url+'/api/report', data=json.dumps(data), headers=headers)
@@ -14,5 +14,10 @@ s = data['auth_id']
 while count <= 10:
     data['auth_id'] += str(count)
     poll = requests.post(url+'/api/poll', data=json.dumps(data), headers=headers)
+    response = json.loads(poll.text)
+    
+    print 'new messages'
+    for x in response:
+    	print 'message:%s target:%s' %(x['message'],x['target'])
     time.sleep(1)
     count += 1    
